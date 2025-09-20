20484734c8a7939acd4544aceb5df27d7b3d5b3c
Lucas de Alencar <lucasalvesalencar3@gmail.com>
2025-09-20 17:58:45 -0300

    simulador de descontps

--- ex04.py
+++ ex04.py
@@ -7,7 +7,7 @@ preço_total = preço_unitario * quantidade
 desconto = float(input('Digite a porcetagem de desconto do produto: '))
 preço_com_desconto = preço_total -(preço_total * desconto)/100
 
-parcelas = int(input('digite  em quantas parcelas quer realizar essa compra: '))
+parcelas = int(input('digite  em quantas parcelas quer realizar essa compra?: '))
 valor_parcelas = preço_com_desconto / parcelas
 print('Valor final da compra:')
 print('produto:', produto)
@@ -15,3 +15,4 @@ print('preço total sem desconto: R$', preço_total)
 print('preço com desconto: R$', preço_com_desconto)
 print('Quantidade de parcelas:', parcelas)
 print('Valor de das parcelas: R$', valor_parcelas)
+
